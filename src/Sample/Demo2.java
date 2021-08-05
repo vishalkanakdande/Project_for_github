8a8e7409fc66fc8f15d0fc91178432fd7daae0b1
Shree <Shree@Shree-PC>
2021-08-06 00:07:50 +0530

    [Vishal] github project 2

--- src/Sample/Demo2.java
+++ src/Sample/Demo2.java
@@ -1,10 +1,11 @@
 package Sample;
 
-public class Demo1 {
+public class Demo2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hi...");
+		System.out.println("Hello.....");
+		
 		
 	}
 	
